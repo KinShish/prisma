65f948f306429d49af1d38d91b1f17f06f9df9d0
kinshish <shishkinb13@gmail.com>
2021-09-03 21:36:58 +0300

    Убрал JWT

--- api/server.js
+++ api/server.js
@@ -2,20 +2,8 @@ const Hapi = require('@hapi/hapi');
 const hapiAuthJWT = require('hapi-auth-jwt2');
 const ip = require("ip");
 const logger=(pool,id_manager,id_company,type,description,url,params)=>{
-    pool.query("INSERT INTO vtr_logger (id_manager,id_company,type,description,url,params) VALUES (?, ?, ?, ?,?,?)",[id_manager,id_company,type,description,url,JSON.stringify(params)]);
     console.log(id_manager,id_company,type,description,url)
-}//1
-const validate = async function (decoded, req, h) {
-    const pool = req.mysql.pool;
-    await pool.query("UPDATE vtr_session SET lastAuth=NOW() WHERE `key`=?",[decoded.key]);
-    const [rows] = await pool.query("SELECT data FROM vtr_session WHERE `key`=?",[decoded.key]);
-    if (rows.length>0) {
-        return { isValid: true ,credentials:rows[0].data};
-    }
-    else {
-        return { isValid: false};
-    }
-};
+}
 
 const init = async (configs) => {
     const host = ip.address() || configs.server.host;
@@ -49,13 +37,6 @@ const init = async (configs) => {
             return h.continue;
         }
     });
-    await server.register(hapiAuthJWT);
-    server.auth.strategy('jwt', 'jwt',
-        { key: configs.modules.key,
-            validate: validate,
-            //verifyOptions: { algorithms: [ 'HS256' ] }
-        });
-    server.auth.default('jwt');
     return server;
 };
 module.exports={init};
